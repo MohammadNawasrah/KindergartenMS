fd9f221a6f8d0120de2ace7e19a9dc7a193a30c9
Mohammad Al Nawasrah <nawasrahmohammad2000@gmail.com>
2023-04-28 03:11:35 +0300

    add file spring boot Init

---
+++ src/main/java/com/nawasrah/schoolMS/SchoolMsApplication.java
@@ -0,0 +1,13 @@
+package com.nawasrah.schoolMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nawasrah/schoolMS/SchoolMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nawasrah.schoolMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
