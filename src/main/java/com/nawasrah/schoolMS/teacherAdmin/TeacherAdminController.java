79ca561a14bff6540c2f451b8c647c28ef25a377
Mohammad Al Nawasrah <nawasrahmohammad2000@gmail.com>
2023-05-15 20:33:23 +0300

    add login techar and admin

--- src/main/java/com/nawasrah/schoolMS/teacherAdmin/TeacherAdminController.java
+++ src/main/java/com/nawasrah/schoolMS/teacherAdmin/TeacherAdminController.java
@@ -1,9 +1,15 @@
 package com.nawasrah.schoolMS.teacherAdmin;
+
 import com.nawasrah.schoolMS.admin.AdminService;
+import com.nawasrah.schoolMS.core.RedirectTo;
+import com.nawasrah.schoolMS.core.ToView;
 import com.nawasrah.schoolMS.teacher.TeacherService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
@@ -16,33 +22,28 @@ public class TeacherAdminController {
     @GetMapping("/")
     public ModelAndView loginPage() {
         System.out.println("Get/index");
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("/index");
-        return modelAndView;
+        return ToView.toView("index");
     }
 
     @PostMapping("/login")
     public RedirectView processLoginForm(@RequestParam("username") String username, HttpSession session,
                                          @RequestParam("password") String password, RedirectAttributes redirectAttributes) {
         System.out.println("Post/login");
-        ModelAndView modelAndView = new ModelAndView();
         Boolean authenticatedUser = teacherService.login(username, password);
         if (authenticatedUser) {
             session.setAttribute("isLogin", true);
             session.setAttribute("userName", username);
-           return AdminService.toAdminView();
+            if (AdminService.isAdmin(password))
+                return RedirectTo.redirectTo("/admin_panel");
+            return RedirectTo.redirectTo("/student/student_info");
         }
         redirectAttributes.addFlashAttribute("userName",username);
-        return   new RedirectView("/");
+        return RedirectTo.redirectTo("/");
     }
     @GetMapping("/logout")
     public ModelAndView logout(HttpSession session) {
         System.out.println("Get/logout");
-        ModelAndView modelAndView = new ModelAndView();
         session.invalidate();
-        modelAndView.setViewName("/index");
-        return modelAndView;
+        return ToView.toView("index");
     }
-
-
 }
