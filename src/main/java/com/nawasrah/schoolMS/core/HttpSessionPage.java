bd00549bb112727c7e9a5d5603b1ff202e17ce46
Mohammad Al Nawasrah <nawasrahmohammad2000@gmail.com>
2023-05-17 19:43:49 +0300

    add new cor function

--- src/main/java/com/nawasrah/schoolMS/core/HttpSessionPage.java
+++ src/main/java/com/nawasrah/schoolMS/core/HttpSessionPage.java
@@ -1,2 +1,15 @@
-package com.nawasrah.schoolMS.core;public class HttpSession {
+package com.nawasrah.schoolMS.core;
+
+import jakarta.servlet.http.HttpSession;
+
+public class HttpSessionPage {
+    public static Object getSession(HttpSession session,String key) {
+        return session.getAttribute(key);
+    }
+    public static void setSession(HttpSession session,String key,Object value) {
+        session.setAttribute(key,value);
+    }
+    public static void closeSession(HttpSession session){
+        session.invalidate();
+    }
 }
