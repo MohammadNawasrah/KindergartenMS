d6bf8c82921ffe3264442fc5a5da60868310b09f
Mohammad Al Nawasrah <nawasrahmohammad2000@gmail.com>
2023-05-17 19:45:16 +0300

    modify toview core

--- src/main/java/com/nawasrah/schoolMS/core/ToView.java
+++ src/main/java/com/nawasrah/schoolMS/core/ToView.java
@@ -1,11 +1,16 @@
 package com.nawasrah.schoolMS.core;
-
 import org.springframework.web.servlet.ModelAndView;
 
 public class ToView {
     public static ModelAndView toView(String nameOfView) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("/" + nameOfView);
+        return modelAndView;
+    }
+    public static ModelAndView toView(String nameOfView,String attributeName,Object object) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/" + nameOfView);
+        modelAndView.addObject(attributeName,object);
         return modelAndView;
     }
 }
